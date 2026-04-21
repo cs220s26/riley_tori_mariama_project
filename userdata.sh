7e48b301037f145928e66d89363483769cadc051
Tori Champagne <champagnet@moravian.edu>
2026-04-21 13:16:14 -0400

    update userdata.sh to compile code

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,8 @@ yum install git -y
 git clone https://github.com/cs220s26/riley_tori_mariama_ahmed
 cd /riley_tori_mariama_ahmed
 python3 -m venv .venv
-.venv/bin/pip install -r requirements.txt 
+source .venv/bin/activate
+mvn package
 cp discord_bot.service /etc/systemd/system
 systemctl enable discord_bot.service 
 systemctl start discord_bot.service
