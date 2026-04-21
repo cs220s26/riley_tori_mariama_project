67efa3b4c0af3927d75f4aa9cd8b7a16558a6938
RileyTrigo <rileytrigo@gmail.com>
2026-04-21 13:21:46 -0400

    remove virtual enviorment

--- userdata.sh
+++ userdata.sh
@@ -2,8 +2,6 @@
 yum install git redis6 maven-amazon-corretto21 -y
 git clone https://github.com/cs220s26/riley_tori_mariama_ahmed
 cd /riley_tori_mariama_ahmed
-python3 -m venv .venv
-source .venv/bin/activate
 mvn package
 cp discord_bot.service /etc/systemd/system
 systemctl enable discord_bot.service 
