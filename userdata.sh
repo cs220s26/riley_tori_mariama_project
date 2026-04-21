288d9c26650a97847c025aee6fcc5a26aa910f65
RileyTrigo <rileytrigo@gmail.com>
2026-04-21 14:22:11 -0400

    fix typos

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-yum install git redis6 maven-amazon-corretto21 -y
-git clone https://github.com/cs220s26/riley_tori_mariama_ahmed
+sudo yum install -y git redis6 maven-amazon-corretto21
+git clone https://github.com/cs220s26/riley_tori_mariama_ahmed.git
 cd /riley_tori_mariama_ahmed
-mvn package
+sudo mvn package
 cp discord_bot.service /etc/systemd/system
 sudo systemctl start redis6
 sudo systemctl enable discord_bot.service 
